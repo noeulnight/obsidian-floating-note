bfe1885d521a4c31bd3e16ea1de4f97fad4f6cab
Limtaehyun <admin@limtaehyun.dev>
2025-12-24 10:30:20 +0900

    feat: floating note window

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,89 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Menu, Plugin } from "obsidian";
+import {
+	DEFAULT_SETTINGS,
+	FloatingNoteSettings,
+	FloatingNoteSettingTab,
+} from "./settings";
+import remote from "@electron/remote";
 
-// Remember to rename these classes and interfaces!
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FloatingNote extends Plugin {
+	settings: FloatingNoteSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
+			id: "open-floating-note-window",
+			name: "Open floating note window",
+			icon: "popup-open",
+			callback: () => this.createLeafWindow(),
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
+		this.addRibbonIcon("popup-open", "Open floating note window", () => {
+			void this.createLeafWindow();
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+		this.addSettingTab(new FloatingNoteSettingTab(this.app, this));
 	}
 
-	onunload() {
+	async createLeafWindow() {
+		const existingWindowIds = remote.BrowserWindow.getAllWindows().map(
+			(win) => win.id
+		);
+
+		const leafWindow = this.app.workspace.getLeaf("window");
+
+		this.app.workspace.setActiveLeaf(leafWindow, { focus: true });
+
+		const newWindowIds = remote.BrowserWindow.getAllWindows().map(
+			(win) => win.id
+		);
+		const createdWindowId = newWindowIds.find(
+			(id) => !existingWindowIds.includes(id)
+		);
+
+		if (createdWindowId) {
+			const electronWindow = remote.BrowserWindow.fromId(createdWindowId);
+			if (electronWindow) {
+				electronWindow.setAlwaysOnTop(true, "screen-saver");
+				electronWindow.setOpacity(0.95);
+				await electronWindow.webContents.executeJavaScript(`
+						// Hide individual tab buttons
+						const tabButtons = document.querySelectorAll('.workspace-tab-header');
+						tabButtons.forEach(tab => {
+							tab.style.display = 'none';
+						});
+
+						// Hide new tab button and other controls
+						const newTabButtons = document.querySelectorAll('.workspace-tab-header-new-tab');
+						newTabButtons.forEach(btn => {
+							btn.style.display = 'none';
+						});
+
+						// Minimize tab header height but keep it draggable
+						const tabContainers = document.querySelectorAll('.workspace-tab-header-container');
+						tabContainers.forEach(container => {
+							container.style.minHeight = '40px';
+							container.style.height = '40px';
+							container.style.backgroundColor = 'transparent';
+							container.style.borderBottom = 'none';
+						});
+					`);
+			}
+		}
 	}
+
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			(await this.loadData()) as Partial<FloatingNoteSettings>
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
